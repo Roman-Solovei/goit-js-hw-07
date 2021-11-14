782137841997631ed5f67b897b252a7df2fcb8a4
Roman-Solovei <romario2409@gmail.com>
2021-11-14 17:01:17 +0200

    hw 07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
-
-
 const galleryContainer = document.querySelector('.gallery');
-console.log(galleryContainer);
 
-const galleryMarkup = createGalleryItems(galleryItems);
-console.log(galleryMarkup);
+const galleryMarkup = onCreateGalleryItems(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// galleryContainer.addEventListener('click', onPaletteContainerClick);
-
-
-function createGalleryItems(images) {
-    // console.log(images)
+function onCreateGalleryItems(images) {   
     return images
       .map(({ preview, original, description }) => {
         return `
@@ -32,118 +23,33 @@ function createGalleryItems(images) {
     </div>`;     
       })
       .join('');
+};  
+
+galleryContainer.addEventListener('click', onClickHandler);
+
+function onClickHandler(e) { 
+  e.preventDefault();
+   
+  if (e.target.nodeName === 'IMG') {        
+    basicLightbox.create(`
+        <img class=modal width="1280" src=${e.target.dataset.source}>     
+      `).show();
+  };  
+ 
 };
-  
-// import * as basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
-
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-
-// instance.show();
-
-
-// // const basicLightbox = require('./basicLightbox');
-// console.dir(basicLightbox);
-// const qwer = basicLightbox.open;
-// console.log(qwer);
-
-
-// const qqqq = document.querySelector('.gallery__item');
 
 
 
-// function swww() {
-//     basicLightbox.create(`
-// 		<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg">
-// 	`).show()
-// }
-//     ;
-// qqqq.addEventListener('click', swww());
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
+galleryContainer.addEventListener('keydown', onEscKeyPress);
 
-// instance.show()
+function onEscKeyPress(e) {
+  console.log(e.code)
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = e.code === ESC_KEY_CODE;
 
-// import * as basicLightbox from 'https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js';
-
-// const basicLightbox = require('https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js');
-// const instance = basicLightbox.create((images) => {
-//     return images
-//       .map(({ preview, original, description }) => {
-//         return `
-//     <div class="gallery__item">
-//     <a class="gallery__link" href="${original}">
-//     <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//      />
-//     </a>
-//     </div>`;     
-//       })
-//       .join('');
-// });
-
-// instance.show()
-// const instance = basicLightbox.create(
-//     document.querySelector('.gallery__image')
-// );
-// console.log(basicLightbox);
-
-
-// const instance = basicLightbox.create(`
-//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="800" height="600">
-// `)
-
-// instance.show();
-// const galleryItem = document.body;
-// console.log(galleryItem);
-
-
-// // const instance =
-// //     basicLightbox.create(`
-// //     <div class="modal">
-// //         <p>
-// //             Your first lightbox with just a few lines of code.
-// //             Yes, it's really that simple.
-// //         </p>
-// //     </div>
-// // `);
-//   const instance =
-//     basicLightbox.create(`
-//     <div class="modal">
-//         <p>
-//             Your first lightbox with just a few lines of code.
-//             Yes, it's really that simple.
-//         </p>
-//     </div>
-// `);
-//     instance.show();
-//     // console.log(instance);
-// function rew() {
-//     const er = instance;
-//     console.log(er);
-  
-// };
-// galleryItem.addEventListener('click', rew());
-// instance.show();
-
-
-// const content = document.createElement('div')
-
-// const h1 = document.createElement('h1')
-// const p = document.createElement('p')
-
-// h1.textContent = 'Create element'
-// p.textContent = 'Create elements dynamicly and use them in your lightbox.'
-
-// content.appendChild(h1)
-// content.appendChild(p)
-
-// const instance = basicLightbox.create(content)
-
-// document.querySelector('.create').onclick = instance.show
+  if (isEscKey) {
+    const closeBasicLightbox = document.querySelector('.modal');
+    console.log(closeBasicLightbox);
+    closeBasicLightbox.classList.remove('.modal');   
+  };
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = onCreateGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function onCreateGalleryItems(images) {
+  console.log()
+    return images
+      .map(({ preview, original, description }) => {
+        return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;     
+      })
+      .join('');
+  
+};
+
+
+var lightbox = new SimpleLightbox('.gallery a');
+
+galleryContainer.addEventListener('click', onClickHandler);
+
+function onClickHandler(e) {
+  e.preventDefault();
+  
+// lightbox.options.captionData = `${e.target.getAttribute('alt')}`;
+//  console.log(lightbox.options.captionData);
+     
+  lightbox.options.captionSelector = `${e.target.getAttribute('alt')}`;
+  console.log(lightbox.options.captionSelector);
+  
+  lightbox.options.captionDelay = 250;
+  lightbox.options.closeText = 'false';
+};
