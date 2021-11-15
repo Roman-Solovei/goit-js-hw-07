2d294d0835293b348dd34f8c9562ed6c73ec0d3d
Roman-Solovei <romario2409@gmail.com>
2021-11-15 22:12:47 +0200

    hw 07 v2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,13 +15,9 @@ function onCreateGalleryItems(images) {
       .map(({ preview, original, description }) => {
         return `
     <a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>`;     
-      })
-      .join('');
-  
+    <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;     
+      }).join('');  
 };
-
 
 var lightbox = new SimpleLightbox('.gallery a');
 
@@ -29,13 +25,7 @@ galleryContainer.addEventListener('click', onClickHandler);
 
 function onClickHandler(e) {
   e.preventDefault();
-  
-// lightbox.options.captionData = `${e.target.getAttribute('alt')}`;
-//  console.log(lightbox.options.captionData);
-     
-  lightbox.options.captionSelector = `${e.target.getAttribute('alt')}`;
-  console.log(lightbox.options.captionSelector);
-  
+  lightbox.options.captionsData = "alt";  
   lightbox.options.captionDelay = 250;
-  lightbox.options.closeText = 'false';
+  console.log(lightbox.options.captionsData);
 };
